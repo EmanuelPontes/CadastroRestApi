29efef06ccf2f2db113b6e9d95ef316037bb9f83
Ivan Emanuel Prodelik Pontes <emanuelpp26@gmail.com>
2021-01-04 22:01:10 -0300

    Criação biblioteca para manipular tabelas html, processo de consultar usuários cadastrados finalizado, melhorias no formulário de cadastro

---
+++ public/js/htmlTableUtil.js
@@ -0,0 +1,47 @@
+/**
+ * 
+ * @param {*} tableMatrix = [[headers], [values], [values], [values]]
+ */
+function createTable(tableMatrix) {
+    var table = document.createElement("table");
+    var headerCreated = false;
+    for (let line of tableMatrix) {
+        if (headerCreated == false) {
+            createTableHeader(table, line);
+            headerCreated = true;
+        } else {
+            insertTableValues(table, line);
+        }
+    }
+
+    return table;
+}
+
+function createTableHeader(tableObj, keyList) {
+    var headerRow = tableObj.insertRow(-1);
+    for (let key of keyList) {
+        var tHeader = document.createElement("th");
+        tHeader.innerHTML = key;
+        headerRow.appendChild(tHeader);
+    }
+    tableObj.appendChild(headerRow);
+}
+
+function insertTableValues(tableObj, valueList) {
+    var row = tableObj.insertRow(-1);
+    for (let value of valueList) {
+        var cell = row.insertCell(-1);
+        cell.innerHTML = value;
+    }
+}
+
+function insertRowValue(rowObj, value) {
+    var cell = rowObj.insertCell(-1);
+    cell.innerHTML = value;
+}
+
+function insertRowHeaderValue(rowObj, value) {
+    var cellHeader = document.createElement("th");
+    cellHeader.innerHTML = value;
+    rowObj.appendChild(cellHeader);
+}
